a53eb86e37614fd5c9d446b6e815f2b6a7dc2115
Phil Arfuso <arfusop@gmail.com>
2022-10-18 22:55:04 -0400

    pricing page

--- components/views/pricing/index.tsx
+++ components/views/pricing/index.tsx
@@ -1,32 +1,57 @@
-import { Card, Timeline } from 'antd'
+import { useRouter } from 'next/router'
 
 import styles from './Pricing.module.scss'
 
-const Separator = () => (
-    <svg
-        className={styles.separator}
-        // width="100%"
-        // height="120"
-        viewBox="0.1 0.1 180 40"
-        preserveAspectRatio="none">
-        <g transform="translate(-18.298844,-77.973964)">
-            <path
-                // style="fill:#b5002b;"
-                d="M 31.615583,86.351641 H 192.16499 v 26.901969 c 0,0 -32.03411,-14.237983 -59.62682,-12.72484 -22.34188,1.2252 -54.779359,9.72634 -54.779359,9.72634 0,0 -22.029534,3.62882 -34.471238,-1.88988 -12.441702,-5.51871 -11.67199,-22.013589 -11.67199,-22.013589 z"
-            />
-            <path
-                // style="fill:#ff1a51;"
-                d="M 18.441597,78.106256 H 198.58126 v 39.288614 c 0,0 -43.10672,-27.825245 -73.47599,-19.687823 -30.369264,8.137423 -46.832208,12.548653 -46.832208,12.548653 0,0 -32.775418,8.05972 -46.735258,0 C 17.577964,102.19598 18.441597,78.106256 18.441597,78.106256 Z"
-            />
-        </g>
-    </svg>
-)
-
 const Pricing = () => {
+    const router = useRouter()
+
+    const onQuoteClick = () => router.push('/contact')
+
     return (
         <section className={styles.PricingPageContent}>
-            <h2>Pricing and Packages</h2>
-            <Separator />
+            <h2>OUR PRICING</h2>
+            <div className={`${styles.mainServiceCard} ${styles.baseCard}`}>
+                <div className={styles.cardContent}>
+                    <div className={styles.priceDisplay}>
+                        <span className={styles.priceMessage}>
+                            *LIMITED TIME OFFER*
+                        </span>
+                        <div className={styles.prices}>
+                            <span className={styles.strikethroughPrice}>
+                                $2500
+                            </span>
+                            <span className={styles.limitedPrice}>$1500</span>
+                        </div>
+                    </div>
+                    <div className={styles.packageItems}>
+                        <div className={styles.included}>
+                            <h4>This pricing includes the following</h4>
+                            <ul>
+                                <li>Home Page</li>
+                                <li>Content Management System (CMS)</li>
+                                <li>Newsletter</li>
+                            </ul>
+                        </div>
+                        <div className={styles.divider} />
+                        <div className={styles.optional}>
+                            <h4>With your choice of 3 of these</h4>
+                            <ul>
+                                <li>Services</li>
+                                <li>Pricing & Packages</li>
+                                <li>Testimonials</li>
+                                <li>Blog</li>
+                                <li>Contact Form/Calendar</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles.additionalPricingInfo}>
+                        Above is our standard package, which suites most of our
+                        clients. If you are interested in a more customized
+                        package
+                    </div>
+                    <button onClick={onQuoteClick}>Get your quote</button>
+                </div>
+            </div>
         </section>
     )
 }
