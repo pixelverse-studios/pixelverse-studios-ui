e64f69d0206cf0c3fd895e37a4e7b8791a16cb4b
Phil Arfuso <arfusop@gmail.com>
2022-10-19 22:28:24 -0400

    max-width sizing

--- components/views/pricing/index.tsx
+++ components/views/pricing/index.tsx
@@ -9,47 +9,51 @@ const Pricing = () => {
 
     return (
         <section className={styles.PricingPageContent}>
-            <h2>OUR PRICING</h2>
-            <div className={`${styles.mainServiceCard} ${styles.baseCard}`}>
-                <div className={styles.cardContent}>
-                    <div className={styles.priceDisplay}>
-                        <span className={styles.priceMessage}>
-                            *LIMITED TIME OFFER*
-                        </span>
-                        <div className={styles.prices}>
-                            <span className={styles.strikethroughPrice}>
-                                $2500
+            <div className="contentSize">
+                <h2>OUR PRICING</h2>
+                <div className={`${styles.mainServiceCard} ${styles.baseCard}`}>
+                    <div className={styles.cardContent}>
+                        <div className={styles.priceDisplay}>
+                            <span className={styles.priceMessage}>
+                                *LIMITED TIME OFFER*
                             </span>
-                            <span className={styles.limitedPrice}>$1500</span>
+                            <div className={styles.prices}>
+                                <span className={styles.strikethroughPrice}>
+                                    $2500
+                                </span>
+                                <span className={styles.limitedPrice}>
+                                    $1500
+                                </span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.packageItems}>
-                        <div className={styles.included}>
-                            <h4>This pricing includes the following</h4>
-                            <ul>
-                                <li>Home Page</li>
-                                <li>Content Management System (CMS)</li>
-                                <li>Newsletter</li>
-                            </ul>
+                        <div className={styles.packageItems}>
+                            <div className={styles.included}>
+                                <h4>This pricing includes the following</h4>
+                                <ul>
+                                    <li>Home Page</li>
+                                    <li>Content Management System (CMS)</li>
+                                    <li>Newsletter</li>
+                                </ul>
+                            </div>
+                            <div className={styles.divider} />
+                            <div className={styles.optional}>
+                                <h4>With your choice of 3 of these</h4>
+                                <ul>
+                                    <li>Services</li>
+                                    <li>Pricing & Packages</li>
+                                    <li>Testimonials</li>
+                                    <li>Blog</li>
+                                    <li>Contact Form/Calendar</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className={styles.divider} />
-                        <div className={styles.optional}>
-                            <h4>With your choice of 3 of these</h4>
-                            <ul>
-                                <li>Services</li>
-                                <li>Pricing & Packages</li>
-                                <li>Testimonials</li>
-                                <li>Blog</li>
-                                <li>Contact Form/Calendar</li>
-                            </ul>
+                        <div className={styles.additionalPricingInfo}>
+                            Above is our standard package, which suites most of
+                            our clients. If you are interested in a more
+                            customized package
                         </div>
+                        <button onClick={onQuoteClick}>Get your quote</button>
                     </div>
-                    <div className={styles.additionalPricingInfo}>
-                        Above is our standard package, which suites most of our
-                        clients. If you are interested in a more customized
-                        package
-                    </div>
-                    <button onClick={onQuoteClick}>Get your quote</button>
                 </div>
             </div>
         </section>
--- components/views/services/index.tsx
+++ components/views/services/index.tsx
@@ -20,38 +20,40 @@ const Services = () => {
     return (
         <section className={styles.ServicesPageContent}>
             <div className={styles.contentBlock}>
-                <h1>LEARN MORE ABOUT OUR SERVICES</h1>
-                <div className={styles.mainContentBlock}>
-                    <p>
-                        We hold ourselves to the highest standards, utilizing
-                        only the best practices to create a custom website that
-                        you can be proud of.
-                    </p>
-                    <div className={styles.serviceDiagram}>
-                        <Card
-                            className={`${styles.mainServiceCard} ${styles.baseCard}`}>
-                            Custom Web Development
-                        </Card>
-                        <div className={styles.subServices}>
-                            <div className={styles.subServiceGroup}>
-                                <Card
-                                    className={`${styles.subServiceCard} ${styles.baseCard}`}>
-                                    Clean, modern UI
-                                </Card>
-                                <Card
-                                    className={`${styles.subServiceCard} ${styles.baseCard}`}>
-                                    Simple, streamlined UX
-                                </Card>
-                            </div>
-                            <div className={styles.subServiceGroup}>
-                                <Card
-                                    className={`${styles.subServiceCard} ${styles.baseCard}`}>
-                                    Mobile responsive pages
-                                </Card>
-                                <Card
-                                    className={`${styles.subServiceCard} ${styles.baseCard}`}>
-                                    Content management system
-                                </Card>
+                <div className="contentSize">
+                    <h1>LEARN MORE ABOUT OUR SERVICES</h1>
+                    <div className={styles.mainContentBlock}>
+                        <p>
+                            We hold ourselves to the highest standards,
+                            utilizing only the best practices to create a custom
+                            website that you can be proud of.
+                        </p>
+                        <div className={styles.serviceDiagram}>
+                            <Card
+                                className={`${styles.mainServiceCard} ${styles.baseCard}`}>
+                                Custom Web Development
+                            </Card>
+                            <div className={styles.subServices}>
+                                <div className={styles.subServiceGroup}>
+                                    <Card
+                                        className={`${styles.subServiceCard} ${styles.baseCard}`}>
+                                        Clean, modern UI
+                                    </Card>
+                                    <Card
+                                        className={`${styles.subServiceCard} ${styles.baseCard}`}>
+                                        Simple, streamlined UX
+                                    </Card>
+                                </div>
+                                <div className={styles.subServiceGroup}>
+                                    <Card
+                                        className={`${styles.subServiceCard} ${styles.baseCard}`}>
+                                        Mobile responsive pages
+                                    </Card>
+                                    <Card
+                                        className={`${styles.subServiceCard} ${styles.baseCard}`}>
+                                        Content management system
+                                    </Card>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -59,82 +61,87 @@ const Services = () => {
             </div>
             <Separator />
             <div className={styles.contentBlock}>
-                <h1>THE JOURNEY</h1>
-                <div className={styles.jourenyBlock}>
-                    <div className={styles.journeyCards}>
-                        <Card
-                            className={`${styles.journeyCard} ${styles.baseCard}`}>
-                            <span>
-                                What are you looking to accomplish with the
-                                site?
-                            </span>
-                        </Card>
-                        <Card
-                            className={`${styles.journeyCard} ${styles.baseCard}`}>
-                            <span>
-                                What actions are your users intended to
-                                complete? (fill out a form, email for quote,
-                                etc).
-                            </span>
-                        </Card>
-                        <Card
-                            className={`${styles.journeyCard} ${styles.baseCard}`}>
-                            <span>
-                                What is your projected timeframe in regards to
-                                launching your website?
-                            </span>
-                        </Card>
+                <div className="contentSize">
+                    <h1>THE JOURNEY</h1>
+                    <div className={styles.jourenyBlock}>
+                        <div className={styles.journeyCards}>
+                            <Card
+                                className={`${styles.journeyCard} ${styles.baseCard}`}>
+                                <span>
+                                    What are you looking to accomplish with the
+                                    site?
+                                </span>
+                            </Card>
+                            <Card
+                                className={`${styles.journeyCard} ${styles.baseCard}`}>
+                                <span>
+                                    What actions are your users intended to
+                                    complete? (fill out a form, email for quote,
+                                    etc).
+                                </span>
+                            </Card>
+                            <Card
+                                className={`${styles.journeyCard} ${styles.baseCard}`}>
+                                <span>
+                                    What is your projected timeframe in regards
+                                    to launching your website?
+                                </span>
+                            </Card>
+                        </div>
+                        <p>
+                            Once these questions are answered and we touch on
+                            some additional talking points, here is what our
+                            process looks like.
+                        </p>
                     </div>
-                    <p>
-                        Once these questions are answered and we touch on some
-                        additional talking points, here is what our process
-                        looks like.
-                    </p>
                 </div>
             </div>
             <Separator />
             <div className={styles.contentBlock}>
-                <h1>OUR WORK FLOW</h1>
-                <div className={styles.workFlowBlock}>
-                    <p>
-                        The first step in our journey is a kickoff meeting where
-                        you present your vision so we can work together to
-                        accomplish your project in a timely manner.
-                    </p>
-                    <Timeline mode="alternate">
-                        <Timeline.Item>
-                            <span>
-                                Mockup/Wireframe design + Style and theme
-                                creation (with our graphic designer or you can
-                                provide your own assets)
-                            </span>
-                        </Timeline.Item>
-                        <Timeline.Item>
-                            <span>
-                                Code development (according to an agreed upon
-                                time frame and schedule)
-                            </span>
-                        </Timeline.Item>
-                        <Timeline.Item>
-                            <span>
-                                Internal testing and Quality assurance. We want
-                                to make sure the website is flawless before
-                                giving a demo
-                            </span>
-                        </Timeline.Item>
-                        <Timeline.Item>
-                            <span>
-                                Demo website to you, make small tweaks and
-                                changes if needed
-                            </span>
-                        </Timeline.Item>
-                        <Timeline.Item>
-                            <span>Website launch</span>
-                        </Timeline.Item>
-                        <Timeline.Item>
-                            <span>Ongoing maintenance as needed</span>
-                        </Timeline.Item>
-                    </Timeline>
+                <div className="contentSize">
+                    <h1>OUR WORK FLOW</h1>
+                    <div className={styles.workFlowBlock}>
+                        <p>
+                            The first step in our journey is a kickoff meeting
+                            where you present your vision so we can work
+                            together to accomplish your project in a timely
+                            manner.
+                        </p>
+                        <Timeline mode="alternate">
+                            <Timeline.Item>
+                                <span>
+                                    Mockup/Wireframe design + Style and theme
+                                    creation (with our graphic designer or you
+                                    can provide your own assets)
+                                </span>
+                            </Timeline.Item>
+                            <Timeline.Item>
+                                <span>
+                                    Code development (according to an agreed
+                                    upon time frame and schedule)
+                                </span>
+                            </Timeline.Item>
+                            <Timeline.Item>
+                                <span>
+                                    Internal testing and Quality assurance. We
+                                    want to make sure the website is flawless
+                                    before giving a demo
+                                </span>
+                            </Timeline.Item>
+                            <Timeline.Item>
+                                <span>
+                                    Demo website to you, make small tweaks and
+                                    changes if needed
+                                </span>
+                            </Timeline.Item>
+                            <Timeline.Item>
+                                <span>Website launch</span>
+                            </Timeline.Item>
+                            <Timeline.Item>
+                                <span>Ongoing maintenance as needed</span>
+                            </Timeline.Item>
+                        </Timeline>
+                    </div>
                 </div>
             </div>
         </section>
