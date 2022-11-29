02eda3389d0e1379e6d4e13481e697a9de1df35d
Phil Arfuso <arfusop@gmail.com>
2022-11-28 21:33:42 -0500

    fix bug

--- lib/gql/mutations/clients.ts
+++ lib/gql/mutations/clients.ts
@@ -3,8 +3,13 @@ import gql from 'graphql-tag'
 export const ADD_NEW_CLIENT = gql`
     mutation addNewClient($eventUri: String!, $inviteeUri: String!) {
         addNewClient(eventUri: $eventUri, inviteeUri: $inviteeUri) {
-            _id
-            email
+            ... on ClientSuccess {
+                _id
+            }
+            ... on Errors {
+                type
+                message
+            }
         }
     }
 `
