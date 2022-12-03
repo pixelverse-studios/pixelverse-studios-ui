7934453b72ab028965eaa56432e7fa21b0aae1a1
Phil Arfuso <arfusop@gmail.com>
2022-12-03 13:03:58 -0500

    dashboard wrapper

--- components/views/dashboard/index.tsx
+++ components/views/dashboard/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect } from 'react'
+import { useLazyQuery } from '@apollo/client'
+
+import { GET_ALL_USERS } from '../../../lib/gql/queries/user'
+import Loader from '../../loader/triangle'
 import styles from './Dashboard.module.scss'
 
+const DashboardWrapper = ({ children }: { children: any }) => (
+    <section className={styles.Dashboard}>
+        <h1>Clients</h1>
+        {children}
+    </section>
+)
+
 const Dashboard = () => {
+    const [getAllUsers] = useLazyQuery(GET_ALL_USERS, {
+        onCompleted() {},
+        onError() {
+            // dispatch banner with error message
+        }
+    })
+
+    useEffect(() => {
+        // dispatch loading
+    }, [])
+
+    // if (true) {
+    //     return (
+    //         <DashboardWrapper>
+    //             <Loader />
+    //         </DashboardWrapper>
+    //     )
+    // }
+
     return (
-        <section className={styles.Dashboard}>
-            <h1>Clients</h1>
+        <DashboardWrapper>
             <div className={styles.personnelOverview}>
                 <div>CLIENTS OVERVIEW</div>
                 <div>USERS OVERVIEW</div>
@@ -18,7 +48,7 @@ const Dashboard = () => {
                 <div>CLIENT 3</div>
                 <div>CLIENT 4</div>
             </div>
-        </section>
+        </DashboardWrapper>
     )
 }
 
--- lib/gql/queries/user.ts
+++ lib/gql/queries/user.ts
@@ -20,3 +20,24 @@ export const GET_LOGGED_IN_USER = gql`
         }
     }
 `
+
+export const GET_ALL_USERS = gql`
+    query Query {
+        getAllUsers {
+            ... on UserSuccess {
+                _id
+                email
+                password
+                firstName
+                lastName
+                token
+                passwordResetToken
+                successType
+            }
+            ... on Errors {
+                message
+                type
+            }
+        }
+    }
+`
