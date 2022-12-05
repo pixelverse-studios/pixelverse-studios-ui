ebf12e464eb09050fc8a205eb769330322c7ec22
Kevin <kevin.lacarrubba@yahoo.com>
2022-12-05 13:07:19 -0500

    linked to overview

--- components/views/dashboard/components/projectsOverview/index.tsx
+++ components/views/dashboard/components/projectsOverview/index.tsx
@@ -1,14 +1,19 @@
 import { useSelector } from 'react-redux'
 import { Card } from 'antd'
 
+import { useRouter } from 'next/router'
+
 import styles from './ProjectOverview.module.scss'
 
 const ProjectOverview = () => {
+    const router = useRouter()
     const completedProjects = []
     const { projects } = useSelector((state: any) => state.allProjects)
 
     return (
-        <Card className={styles.ProjectOverviewContent}>
+        <Card
+            className={styles.ProjectOverviewContent}
+            onClick={() => router.push('/dashboard/projects/overview')}>
             Projects:{completedProjects.length} / {projects.length} completed
         </Card>
     )
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import Dashboard from '../components/views/dashboard'
+import Dashboard from '../../components/views/dashboard'
 
 const DashboardPage = () => {
     return <Dashboard />
