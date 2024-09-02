4909e341a618713fc72c1a7a1d3eb37d519084b6
Phil Arfuso <arfusop.dev@gmail.com>
2024-09-02 10:16:20 -0400

    remove barrese bookkeeping from works

--- views/works/utils.ts
+++ views/works/utils.ts
@@ -2,13 +2,13 @@ import BarreseBookkeepingImg from '../../assets/works/BarreseBookkeeping.png'
 import GoingGoldChoreography from '../../assets/works/GoingGoldChoreography.png'
 
 export const CLIENT_PAGES = [
-    {
-        company: 'Barrese Bookkeeping LLC',
-        url: 'https://www.barresebookkeeping.com',
-        img: BarreseBookkeepingImg.src,
-        description:
-            'Offering bookkeeping, accounts payable and receivable, and payroll processing services'
-    },
+    // {
+    //     company: 'Barrese Bookkeeping LLC',
+    //     url: 'https://www.barresebookkeeping.com',
+    //     img: BarreseBookkeepingImg.src,
+    //     description:
+    //         'Offering bookkeeping, accounts payable and receivable, and payroll processing services'
+    // },
     {
         company: 'Going Gold Choreography',
         url: 'https://www.goinggoldchoreography.com',
